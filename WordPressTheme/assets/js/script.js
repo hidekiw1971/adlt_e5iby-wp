b0c5b2e18e0bd81adc91d36e7fb77010a92f24c1
hideki watabe <hideki.w.1971@gmail.com>
2023-07-10 03:56:43 +0900

    2023/7/10 3:56 wp化対応

--- WordPressTheme/assets/js/script.js
+++ WordPressTheme/assets/js/script.js
@@ -1,11 +1,50 @@
 "use strict";
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-    MicroModal.init({
-        awaitCloseAnimation: true,
-        awaitOpenAnimation: true,
-        disableScroll: true
+    // 開始
+    // swipre-singe
+    const swiperSingle = new Swiper('.swiper-single', {
+        autoplay: {
+            delay: 1000,
+            disableOnInteraction: false,
+        },
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
     });
-    // 
-});
 
+    // cards-swiper
+    const cardsSwiper = new Swiper('.cards-swiper', {
+        allowTouchMove: false,
+        autoplay: {
+            delay: 1000,
+            disableOnInteraction: false,
+        },
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
+    // accordion
+    window.addEventListener('scroll', function () {
+        var button = document.querySelector('.article-svideo');
+        if (button) {
+            if (window.scrollY > 0) {
+                button.classList.add('hidden');
+            } else {
+                button.classList.remove('hidden');
+            }
+        }
+    });
+    // 終了
+});
--- WordPressTheme/functions.php
+++ WordPressTheme/functions.php
@@ -6,39 +6,42 @@
 add_theme_support('post-thumbnails');
 set_post_thumbnail_size(220, 165, true); // 幅 220px、高さ 165px、切り抜きモード
 
-/* CSSとJavaScriptの読み込み */
 function my_script_init()
-{ // WordPressに含まれているjquery.jsを読み込まない
+{
+	// WordPressに含まれているjquery.jsを読み込まない
 	wp_deregister_script('jquery');
 	// jQueryの読み込み
+	wp_enqueue_script('jquery', '//code.jquery.com/jquery-3.7.0.min.js', array(), '3.7.0', true);
+	wp_enqueue_script('swiper-js', get_template_directory_uri() . '/assets/js/swiper-bundle.min.js', array('jquery'), '0.0.0', true);
+	wp_enqueue_style('swiper-css', get_template_directory_uri() . '/assets/css/swiper-bundle.min.css', array(), '0.0.0');
 	wp_enqueue_style('style-css', get_template_directory_uri() . '/assets/css/styles.css', array(), '0.0.0');
-	wp_enqueue_script('jquery', '//code.jquery.com/jquery-3.6.1.min.js', "", "0.0.0", true);
 	wp_enqueue_script('main-js', get_template_directory_uri() . '/assets/js/script.js', array('jquery'), '0.0.0', true);
-	wp_enqueue_script('polyfill', '//polyfill.io/v3/polyfill.min.js?features=es6', "", "0.0.0", true);
-	wp_enqueue_script('micromodal', '//unpkg.com/micromodal/dist/micromodal.min.js', "", "0.0.0", true);
-	wp_enqueue_style('bootstrap-icons', '//cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css', array(), '0.0.0', 'all');
+	wp_enqueue_script('polyfill', '//polyfill.io/v3/polyfill.min.js?features=es6', array(), '0.0.0', true);
+	wp_enqueue_script('micromodal', '//unpkg.com/micromodal/dist/micromodal.min.js', array(), '0.0.0', true);
+	wp_enqueue_style('bootstrap-icons', '//cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css', array(), '1.8.0', 'all');
 	wp_enqueue_script('bootstrap4-js', get_template_directory_uri() . '/assets/js/bootstrap.bundle.js', array('jquery'), '0.0.0', true);
-	wp_enqueue_style('boootstrap4-css', get_template_directory_uri() . '/assets/css/bootstrap.css', array(), '0.0.0');
+	wp_enqueue_style('bootstrap4-css', get_template_directory_uri() . '/assets/css/bootstrap.css', array(), '0.0.0');
+	wp_enqueue_style('bootstrap4-css-map', get_template_directory_uri() . '/assets/css/bootstrap.css.map', array(), '0.0.0');
 }
 add_action('wp_enqueue_scripts', 'my_script_init');
 
 /**
  * アイキャッチ画像設置時短
  */
-function catch_that_image()
-{
-	global $post, $posts;
-	$first_img = '';
-	ob_start();
-	ob_end_clean();
-	$output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
-	$first_img = $matches[1][0];
-	if (empty($first_img)) {
-		// 記事内で画像がなかったときのためのデフォルト画像を指定
-		$first_img = "https://wtb-web.com/wp-content/uploads/2023/04/noimage-1.png";
-	}
-	return $first_img;
-}
+// function catch_that_image()
+// {
+// 	global $post, $posts;
+// 	$first_img = '';
+// 	ob_start();
+// 	ob_end_clean();
+// 	$output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
+// 	$first_img = $matches[1][0];
+// 	if (empty($first_img)) {
+// 記事内で画像がなかったときのためのデフォルト画像を指定
+// 		$first_img = "https://wtb-web.com/wp-content/uploads/2023/04/noimage-1.png";
+// 	}
+// 	return $first_img;
+// }
 
 // 管理画面のカスタム投稿一覧を日付順で表示する
 function admin_custom_posttype_order($wp_query)
