5927d70c2d827aa9ab19256909d86bb273761acd
hideki watabe <hideki.w.1971@gmail.com>
2023-07-13 04:16:19 +0900

    2023/7/13 4:16

--- src/js/script.js
+++ src/js/script.js
@@ -45,5 +45,18 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
       }
     }
   });
+
+  // accordion-sns
+  $(document).ready(function () {
+    $('.accordion-menu-item-btn:first').addClass('accordion-menu-item-btn-onLeft');
+    $('.accordion-menu-item-btn:first').addClass('accordion-menu-item-btn-onRight');
+    $('.accordion-menu-item-btn:first').siblings('ul').stop().slideDown();
+  });
+
+  $('.accordion-menu-item-btn').click(function () {
+    $(this).toggleClass('accordion-menu-item-btn-onLeft');
+    $(this).toggleClass('accordion-menu-item-btn-onRight');
+    $(this).siblings('ul').stop().slideToggle();
+  });
   // 終了
 });
